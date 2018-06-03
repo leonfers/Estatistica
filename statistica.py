c780220df91445c13f1e265e7c5ecc157e961161
leonfers <leonfers@hotmail.com>
2018-06-03 00:07:03 -0300

    trabalho de estatistica

--- statistica.py
+++ statistica.py
@@ -6,13 +6,16 @@
 
 import requests
 import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Para pegar dados online
 # base_url = "http://steamspy.com/api.php?request="
 # response = requests.get(base_url+"all")
 # data = json.loads(response.text)
 
-with open('api.php.json', encoding='utf-8') as json_file:
+with open('api.php.json') as json_file:
     text = json_file.read()
     data = json.loads(text)
 
@@ -85,7 +88,7 @@ class Analyzer(object):
     def get_free(self):
         free = []
         for i in self.lista:
-            if(i.price==9.99):
+            if(60 <=i.price):
                 free.append(i)
         return len(free)
     
@@ -136,24 +139,128 @@ class Analyzer(object):
     
     def get_coeficiente_de_variacao(self):
         return (self.get_desvio_padrao()/self.get_media()) * 100
+    
+    def get_classes(self):
+        self.lista.sort(key=lambda x: x.price)
+        classes= [[],[],[],[],[],[],[],[],[],[]]
+        divisoriasI=[0,60,120,180,240,300,360,420,480,540]
+        divisoriasF=[60,120,180,240,300,360,420,480,540,600]
+        contador = 0
+        for i in self.lista:
+            if(divisoriasI[0]<=i.price<divisoriasF[0]):
+                classes[0].append(i)
+            elif(divisoriasI[1]<=i.price<divisoriasF[1]):
+                classes[1].append(i)
+            elif(divisoriasI[2]<=i.price<divisoriasF[2]):
+                classes[2].append(i)
+            elif(divisoriasI[3]<=i.price<divisoriasF[3]):
+                classes[3].append(i)
+            elif(divisoriasI[4]<=i.price<divisoriasF[4]):
+                classes[4].append(i)
+            elif(divisoriasI[5]<=i.price<divisoriasF[5]):
+                classes[5].append(i)
+            elif(divisoriasI[6]<=i.price<divisoriasF[6]):
+                classes[6].append(i)
+            elif(divisoriasI[7]<=i.price<divisoriasF[7]):
+                classes[7].append(i)
+            elif(divisoriasI[8]<=i.price<divisoriasF[8]):
+                classes[8].append(i)
+            elif(divisoriasI[9]<=i.price<divisoriasF[9]):
+                classes[9].append(i)
+
+
+        return classes
+           
+
+
+
 
 analyzer = Analyzer(games)
 
 print("#### Ferramenta para dados Estatisticos  #######")
 print("######## Referente a Jogos da Steam  ###########")
+print("################################################")
+print("Tamanho da amostral:######## %d" % analyzer.get_size())
+print("Minimo:##################### %.2f" % analyzer.get_min_price())
+print("Maximo:##################### %.2f" % analyzer.get_max_price())
+print("Amplitude:################## %.2f" % analyzer.get_amplitude())
+print("Media:###################### %.2f" % analyzer.get_media())
+print("Mediana:#################### %.2f" % analyzer.get_mediana())
+print("Moda:####################### %.2f" % analyzer.get_modinha())
+print("Variancia^2:################ %.2f" % analyzer.get_variancia())
+print("Desvio padrao:############## %.4f" % analyzer.get_desvio_padrao())
+print("Coeficiente de variacao:#### %.2f%%" % analyzer.get_coeficiente_de_variacao())
+print("################################################")
 
-print("jogos gratis: %d" % analyzer.get_free())
-print("Quantidade de faixa de precos: %d "% len(analyzer.get_matrix()))
+divisoriasI=[0,60,120,180,240,300,360,420,480,540]
+divisoriasF=[60,120,180,240,300,360,420,480,540,600]
 
-print("Tamanho da amostral: %d" % analyzer.get_size())
-print("Minimo: %.2f" % analyzer.get_min_price())
-print("Maximo:%.2f" % analyzer.get_max_price())
-print("Amplitude: %.2f" % analyzer.get_amplitude())
-print("Media: %.2f" % analyzer.get_media())
-print("Mediana : %.2f" % analyzer.get_mediana())
-print("Moda: %.2f" % analyzer.get_modinha())
-print("Variancia^2: %.2f" % analyzer.get_variancia())
-print("Desvio padrao: %.4f" % analyzer.get_desvio_padrao())
-print("Coeficiente de variacao: %.2f%%" % analyzer.get_coeficiente_de_variacao())
+classes = analyzer.get_classes()
 
 
+print("   Clases   | Frequencias ")
+for i in range(len(divisoriasF)):
+    print("|  %d  -  %d  | %d " %(divisoriasI[i],divisoriasF[i],len(classes[i])))
+
+games_classes = []
+frequencias_acumuladas = len(games)
+
+for i in classes[0]:
+    obj = Game("",0,60/2)
+    games_classes.append(obj)
+for i in classes[1]:
+    obj = Game("",0,180/2)
+    games_classes.append(obj)
+for i in classes[2]:
+    obj = Game("",0,300/2)
+    games_classes.append(obj)
+for i in classes[3]:
+    obj = Game("",0,420/2)
+    games_classes.append(obj)
+for i in classes[4]:
+    obj = Game("",0,540/2)
+    games_classes.append(obj)
+for i in classes[5]:
+    obj = Game("",0,660/2)
+    games_classes.append(obj)
+for i in classes[6]:
+    obj = Game("",0,780/2)
+    games_classes.append(obj)
+for i in classes[7]:
+    obj = Game("",0,900/2)
+    games_classes.append(obj)
+for i in classes[8]:
+    obj = Game("",0,1020/2)
+    games_classes.append(obj)
+for i in classes[9]:
+    obj = Game("",0,1140/2)
+    games_classes.append(obj)
+
+analyzer2=Analyzer(games_classes)
+
+
+
+
+print("Media:###################### %.2f" % analyzer2.get_media())
+print("Mediana:#################### %.2f" % analyzer2.get_mediana())
+print("Moda:####################### %.2f" % analyzer2.get_modinha())
+print("Variancia^2:################ %.2f" % analyzer2.get_variancia())
+print("Desvio padrao:############## %.4f" % analyzer2.get_desvio_padrao())
+print("Coeficiente de variacao:#### %.2f%%" % analyzer2.get_coeficiente_de_variacao())
+
+
+print("\n")
+print("Pela comparacao da media e da mediana, no caso a minha ")
+print("selecao de dados teve uma grande mudanca, mas isso ocorreu")
+print("devido a alta concentracao na clases")
+print("mas percebes que pela divisao das clases, ambas permaneceram na mesma area")
+
+precos = []
+
+for i in games:
+    precos.append(i.price)
+
+bins = [60,120,180,240,300,360,420,480,540,600]
+
+plt.hist(precos, bins, histtype="bar", rwidth=0.5, label=[0,1,2,3,4,5] )
+plt.show()
